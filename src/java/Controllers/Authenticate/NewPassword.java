fa4bf122644b17318d15966a92965bbf2e377e4c
Nguyen Duc Minh <ndminh1010@gmail.com>
2023-12-03 14:51:17 +0700

    done day02

--- src/java/Controllers/Authenticate/NewPassword.java
+++ src/java/Controllers/Authenticate/NewPassword.java
@@ -19,11 +19,15 @@ public class NewPassword extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         HttpSession session = request.getSession();
+        
+        //get new password of user
         String newPassword = request.getParameter("newPwd");
         
+        //encode password to MD5
         EncodeMD5 encode = new EncodeMD5();
         newPassword = encode.EncoderMD5(newPassword);
         
+        //set new password according to email
         UserDAO uDao = new UserDAO();
         int status = uDao.resetPassWord((String) session.getAttribute("email"),newPassword);
         session.setAttribute("msg", "Reset password susscessful!");
